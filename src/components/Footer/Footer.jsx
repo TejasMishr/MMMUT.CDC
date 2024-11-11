559e5db2d32e7f242fea3c3c9a56ed677c4c412a
LAZY-CODER01 <avinashmaurya430@gmail.com>
2024-11-11 22:35:37 +0530

    Footer images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -34,25 +34,25 @@ export const Footer = () => {
         <ul className="list-none space-y-2">
           <li>
             <a href="https://www.youtube.com/@cdcmmmut" target= "_blank" className="flex items-center text-gray-400 hover:text-white">
-              <img src="http://localhost:5173/socialicons/youtube.png" className="h-5 mr-2" alt="YouTube" />
+              <img src="./socialicons/youtube.png" className="h-5 mr-2" alt="YouTube" />
               <span className="text-lg">YouTube</span>
             </a>
           </li>
           <li>
             <a href="https://www.instagram.com/cdc.mmmut/"target= "_blank" className="flex items-center text-gray-400 hover:text-white">
-              <img src="http://localhost:5173/socialicons/instagram.png" className="h-5 mr-2" alt="Instagram" />
+              <img src="./socialicons/instagram.png" className="h-5 mr-2" alt="Instagram" />
               <span className="text-lg">Instagram</span>
             </a>
           </li>
           <li>
             <a href="https://www.youtube.com/@cdcmmmut" target= "_blank" className="flex items-center text-gray-400 hover:text-white">
-              <img src="http://localhost:5173/socialicons/telegram.png" className="h-5 mr-2" alt="Telegram" />
+              <img src="./socialicons/telegram.png" className="h-5 mr-2" alt="Telegram" />
               <span className="text-lg">Telegram</span>
             </a>
           </li>
           <li>
             <a href="https://www.linkedin.com/company/codechef-mmmut-chapter" target= "_blank" className="flex items-center text-gray-400 hover:text-white">
-              <img src="http://localhost:5173/socialicons/linkedin.png" className="h-5 mr-2" alt="LinkedIn" />
+              <img src="./socialicons/linkedin.png" className="h-5 mr-2" alt="LinkedIn" />
               <span className="text-lg">LinkedIn</span>
             </a>
           </li>
