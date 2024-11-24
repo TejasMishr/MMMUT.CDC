acedd9731e6c876c5edb7d7a87817c7df66706f8
LAZY-CODER01 <avinashmaurya430@gmail.com>
2024-11-25 00:09:05 +0530

    navbar fixed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -26,60 +26,53 @@ export const Navbar = () => {
     <nav className="flex items-center justify-between py-4 px-6 w-full bg-black text-white relative z-50">
       {/* Title */}
       <div className="flex items-center space-x-4">
-        <span className="lg:text-2xl  text-xl font-bold font-poppins tracking-wide">
+        <span className="lg:text-2xl text-xl font-bold font-poppins tracking-wide">
           CODERS & DEVELOPERS CLUB
         </span>
       </div>
 
       {/* Desktop & Tablet Links */}
       <ul className="hidden lg:flex space-x-8 text-lg font-roboto-slab font-semibold">
-        {/* {["HOME", "CONTACT", "TEAM", "EVENTS", "VERIFY"].map((item) => ( */}
-          <li className="group">
-            <NavLink to="/"
-              className=" hover:text-gray-300 transition-colors duration-300 block p-2 "
-            >
-              Home
-            </NavLink>
-
-          </li>
-
-          <li className="group">
-            <NavLink to="events"
-              className="hover:text-gray-300 transition-colors duration-300 block p-2"
-            >
-              Events
-            </NavLink>
-
-          </li>
-
-          <li className="group">
-            <NavLink to="team"
-              className="hover:text-gray-300 transition-colors duration-300 block p-2"
-            >
-              Team
-            </NavLink>
-
-          </li>
-
-          <li className="group">
-            <NavLink to="contact"
-              className="hover:text-gray-300 transition-colors duration-300 block p-2"
-            >
-              Contact
-            </NavLink>
-
-          </li>
-
-          <li className="group">
-            <NavLink to="verify"
-              className="hover:text-gray-300 transition-colors duration-300 block p-2"
-            >
-              DashBoard
-            </NavLink>
-
-          </li>
-
-        {/* ))} */}
+        <li className="group">
+          <NavLink
+            to="/"
+            className="hover:text-gray-300 transition-colors duration-300 block p-2"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="group">
+          <NavLink
+            to="events"
+            className="hover:text-gray-300 transition-colors duration-300 block p-2"
+          >
+            Events
+          </NavLink>
+        </li>
+        <li className="group">
+          <NavLink
+            to="team"
+            className="hover:text-gray-300 transition-colors duration-300 block p-2"
+          >
+            Team
+          </NavLink>
+        </li>
+        <li className="group">
+          <NavLink
+            to="contact"
+            className="hover:text-gray-300 transition-colors duration-300 block p-2"
+          >
+            Contact
+          </NavLink>
+        </li>
+        <li className="group">
+          <NavLink
+            to="verify"
+            className="hover:text-gray-300 transition-colors duration-300 block p-2"
+          >
+            DashBoard
+          </NavLink>
+        </li>
       </ul>
 
       {/* Mobile Menu Button (Visible on mobile & tablet) */}
@@ -92,125 +85,71 @@ export const Navbar = () => {
 
       {/* Login and Dark Mode Toggle - Always Visible */}
       <div className="hidden lg:flex items-center space-x-4">
-        <NavLink to="login"
+        <NavLink
+          to="login"
           className="text-lg font-roboto-slab font-semibold hover:text-gray-300 transition-colors duration-300 block p-2"
         >
           LOGIN
         </NavLink>
-        {/* <label className="relative inline-flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            checked={isDarkMode}
-            onChange={handleToggle}
-            className="sr-only"
-          />
-          <div
-            className={`w-14 h-8 rounded-full shadow-inner relative transition duration-300 ${
-              isDarkMode
-                ? "bg-gradient-to-r from-blue-600 to-purple-600"
-                : "bg-gradient-to-r from-yellow-400 to-sky-500"
-            }`}
-          >
-            <span
-              className={`absolute top-1 left-1 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 flex items-center justify-center ${
-                isDarkMode
-                  ? "translate-x-6 bg-black shadow-blue-500/50"
-                  : "bg-white"
-              }`}
-            >
-              {isDarkMode ? (
-                <FaMoon className="text-blue-300" size={14} />
-              ) : (
-                <FaSun className="text-yellow-500" size={14} />
-              )}
-            </span>
-          </div>
-        </label> */}
       </div>
 
       {/* Mobile & Tablet Menu - Only visible on smaller screens */}
       {isMobileMenuOpen && (
-        <ul className="absolute top-full left-0 w-full h-screen  bg-black bg-opacity-90 text-center py-4 lg:hidden">
-          {/* {["HOME", "CONTACT", "TEAM", "EVENTS", "VERIFY"].map((item) => ( */}
-            <li className="group">
-              <NavLink to="/"
-                className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
-              >
-                Home
-              </NavLink>
-            </li>
-
-            <li className="group">
-              <NavLink to="events"
-                className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
-              >
-                Events
-              </NavLink>
-            </li>
-
-            <li className="group">
-              <NavLink to="team"
-                className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
-              >
-                Team
-              </NavLink>
-            </li>
-
-            <li className="group">
-              <NavLink to="verify"
-                className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
-              >
-                Dashboard
-              </NavLink>
-            </li>
-
-            <li className="group">
-              <NavLink to="contact"
-                className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
-              >
-                Contact
-              </NavLink>
-            </li>
-
-          {/* ))} */}
-          <li>
-             <NavLink to="login"
+        <ul className="absolute top-full left-0 w-full h-screen bg-black bg-opacity-90 text-center py-4 lg:hidden">
+          <li className="group">
+            <NavLink
+              to="/"
               className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="group">
+            <NavLink
+              to="events"
+              className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Events
+            </NavLink>
+          </li>
+          <li className="group">
+            <NavLink
+              to="team"
+              className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Team
+            </NavLink>
+          </li>
+          <li className="group">
+            <NavLink
+              to="contact"
+              className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li className="group">
+            <NavLink
+              to="verify"
+              className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="login"
+              className="text-white text-lg font-roboto-slab hover:text-gray-300 transition-colors duration-300 block py-2"
+              onClick={() => setIsMobileMenuOpen(false)}
             >
               LOGIN
             </NavLink>
           </li>
-          {/* <li>
-            <label className="flex justify-center items-center cursor-pointer py-2">
-              <input
-                type="checkbox"
-                checked={isDarkMode}
-                onChange={handleToggle}
-                className="sr-only"
-              />
-              <div
-                className={`w-14 h-8 rounded-full shadow-inner relative transition duration-300 ${
-                  isDarkMode
-                    ? "bg-gradient-to-r from-blue-600 to-purple-600"
-                    : "bg-gradient-to-r from-yellow-400 to-sky-500"
-                }`}
-              >
-                <span
-                  className={`absolute top-1 left-1 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 flex items-center justify-center ${
-                    isDarkMode
-                      ? "translate-x-6 bg-black shadow-blue-500/50"
-                      : "bg-white"
-                  }`}
-                >
-                  {isDarkMode ? (
-                    <FaMoon className="text-blue-300" size={14} />
-                  ) : (
-                    <FaSun className="text-yellow-500" size={14} />
-                  )}
-                </span>
-              </div>
-            </label>
-          </li> */}
         </ul>
       )}
     </nav>
