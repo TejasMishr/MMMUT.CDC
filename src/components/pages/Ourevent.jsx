8a7a4437fe4ce7251ee54b9f60e91334201dfe5d
TejasMishr <tejasmishra72@gmail.com>
2024-12-15 11:52:39 +0530

    Fixed Event Page

--- src/components/pages/Ourevent.jsx
+++ src/components/pages/Ourevent.jsx
@@ -9,6 +9,13 @@ const eventDetails = [
     ispastevent: false,
   },
   {
+    title: "SYNTAX SIEGE",
+    description:
+      "Syntax Siege is a week-long event featuring five days of offline coding classes designed to enhance programming skills. On the sixth day, participants compete in an online contest, followed by an exciting offline contest on the final day",
+    img: "https://firebasestorage.googleapis.com/v0/b/indi-rp.appspot.com/o/SyntaxSeize%20(1).jpg?alt=media&token=e0fcb88c-610c-411c-ac73-cd1c5910d9a8",
+    ispastevent: true,
+  },
+  {
     title: "CODE CASCADE",
     description:
       "The three-day Code-Cascade event offers online coding classes for juniors, focusing on programming basics in a supportive environment. Participants engage in interactive sessions, exploring key concepts and hands-on activities. The event aims to build coding skills and inspire a passion for technology.",
@@ -20,13 +27,6 @@ const eventDetails = [
     description:
       "Game of Codes is an exclusive event for second-year students, organized by the Coders and Developers Club in collaboration with the Computer Engineering Society. It aims to foster programming skills and innovation among participants. The event promises engaging activities tailored for budding coders.",
     img: "./events/goc.png",
-    ispastevent: true,
-  },
-  {
-    title: "GENESIS",
-    description:
-      "Syntax Siege is a week-long event featuring five days of offline coding classes designed to enhance programming skills. On the sixth day, participants compete in an online contest, followed by an exciting offline contest on the final day",
-    img: "./events/ss.png",
     ispastevent: true,
   },
 ];
@@ -48,18 +48,43 @@ const OurEvents = () => {
           {eventDetails.map((event, index) => (
             <div
               key={index}
-              className="max-w-sm mx-auto bg-gray-800 text-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 hover:shadow-2xl hover:bg-gray-700 cursor-pointer border-[1px] border-gray-700"
+              className="max-w-sm mx-auto bg-gray-900 text-white rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 hover:shadow-2xl hover:bg-gray-800 cursor-pointer border-[1px] border-gray-700"
             >
               <img
                 className="rounded-t-lg w-full object-cover h-52"
                 src={event.img}
                 alt={event.title}
               />
-              <div className="p-5">
-                <h5 className="text-2xl font-bold mb-3">{event.title}</h5>
-                <p className="text-gray-300 text-sm mb-5">
-                  {event.description}
-                </p>
+              <div className=" p-5">
+                <h5 className="text-2xl font-bold mb-3 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent animate-gradient">
+                  {event.title}
+                </h5>
+                <p
+                  className="text-gray-300 text-sm mb-5"
+                  dangerouslySetInnerHTML={{
+                    __html: event.description
+                      .replace(
+                        /ICPC structure/g,
+                        '<span class="text-orange-500 px-1 rounded-sm">ICPC structure</span>'
+                      )
+                      .replace(
+                        /AlgOlympics/g,
+                        '<span class="text-red-500 px-1 rounded-sm">AlgoOlympics</span>'
+                      )
+                      .replace(
+                        /Syntax Siege/g,
+                        '<span class="text-red-500 px-1 rounded-sm">Syntax Siege</span>'
+                      )
+                      .replace(
+                        /Code-Cascade/g,
+                        '<span class="text-red-500 px-1 rounded-sm">Code Cascade</span>'
+                      )
+                      .replace(
+                        /Game of Codes/g,
+                        '<span class="text-red-500 px-1 rounded-sm">Game of Codes</span>'
+                      ),
+                  }}
+                />
                 <div className="flex justify-between items-center">
                   <button
                     className={`px-4 py-2 text-sm rounded-lg cursor-pointer ${
@@ -70,7 +95,7 @@ const OurEvents = () => {
                     onClick={() => {
                       if (!event.ispastevent) {
                         window.open(
-                          "https://cdc-ruby.vercel.app/createTeam",
+                          "https://cdc.mmmut.xyz/createTeam",
                           "_blank"
                         );
                       }
